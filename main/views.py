4fb9257536a32bdcc439fa24417ae2cf0401f595
iDeash123 <istresnaa@gmail.com>
2025-09-23 21:12:06 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,21 @@
+from calendar import c
+from cgitb import text
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {
+        "title": "Home - Головна",
+        "content": "Магазин мебели HOME",
+    }
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст страницы о нас",
+    }
+    return render(request, "main/about.html", context)
