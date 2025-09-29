0a5c932860a0389a182f0a9af654c03f649baed4
iDeash123 <istresnaa@gmail.com>
2025-09-29 20:13:52 +0300

    add first models

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,20 @@
+from os import name
+from tabnanny import verbose
+from django.core.validators import slug_re
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Category name")
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name="URL",
+    )
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "category"
+        verbose_name_plural = "categories"
