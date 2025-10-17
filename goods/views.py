f0071693911657873901a0dae08d4b9b0f3b5c3b
iDeash123 <istresnaa@gmail.com>
2025-10-17 21:35:03 +0300

    top pagination

--- goods/views.py
+++ goods/views.py
@@ -6,15 +6,16 @@ from django.template import context
 from .models import Categories, Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get("page", 1)
 
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
-    paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    paginator = Paginator(goods, 6)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
